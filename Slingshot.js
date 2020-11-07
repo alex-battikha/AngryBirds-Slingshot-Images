fe13ce77ef15d5bd87bb7fefad13e6dc7604a822
alex-battikha <56514843+alex-battikha@users.noreply.github.com>
2020-11-06 18:09:59 -0800

    addeded slingshot images

--- Slingshot.js
+++ Slingshot.js
@@ -9,6 +9,10 @@ class SlingShot{
         this.pointB = pointB
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
+
+        this.sling1 = loadImage("sprites/sling1.png");
+        this.sling2 = loadImage("sprites/sling2.png");
+        this.sling3 = loadImage("sprites/sling3.png");
     }
 
     fly(){
@@ -16,11 +20,17 @@ class SlingShot{
     }
 
     display(){
+        image(this.sling1,190, 70);
+        image(this.sling2,160, 60);
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
-            strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            stroke(48, 22, 8);
+            //push();
+            line(pointA.x, pointA.y, pointB.x+30, pointB.y+20);
+            line(pointA.x, pointA.y, pointB.x-25, pointB.y+20);
+            image(this.sling3, pointA.x-32, pointA.y-10, 15, 30);
+            //pop();
         }
     }
     
